45cc2ef23c50cc47e20f52bfba749480809c2d29
Muhammad Abu Bakar <bc220415372mab@vu.edu.pk>
2026-03-02 03:09:45 +0500

    Route for Dashboard

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get("/", function (){
+    return view("dashboard");
+});
+
 Route::middleware(['auth', 'role:admin'])->prefix('admin')->group(function () {
     Route::view('/dashboard', 'admin.dashboard.index');
 });
